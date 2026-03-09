e8da1f446b1abb2fdf3b1b9d7e0c99acb3af490b
Lucasmourav <124903687+Lucasmourav@users.noreply.github.com>
2026-03-09 08:18:05 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppJogoDaVelha/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Android.Views;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MauiAppJogoDaVelha
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            Window W = new Window(new AppShell());
+            W.Height = 700;
+            W.Width = 350;
+            return W;
+        }
+    }
+}
---
+++ MauiAppJogoDaVelha/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiAppJogoDaVelha
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
