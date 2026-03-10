2ab0233942e17e59caef18208146506d5d066be8
Lucasmourav <124903687+Lucasmourav@users.noreply.github.com>
2026-03-09 21:57:57 -0300

    ok

--- MauiAppJogoDaVelha/MainPage.xaml.cs
+++ MauiAppJogoDaVelha/MainPage.xaml.cs
@@ -19,25 +19,39 @@ namespace MauiAppJogoDaVelha
 
             if (btn10.Text == "X" && btn11.Text == "X" && btn12.Text == "X")
             {
-                await DisplayAlertAsync("Fim de Jogo", "Jogador X venceu!", "OK");
+                await DisplayAlertAsync("🏆", "X Ganhou!", "OK");
             }
 
-            //caminho mais longo para acessar os itens do jogo
-            var itens = Jogo.Children;
 
-            foreach (var x in Jogo.Children)
+            string[,] teste = new string[3, 3];
+
+            // Caminho mais longo...
+            foreach (var x in jogo.Children)
             {
                 if (x is Button btn)
                 {
                     int coluna = Grid.GetColumn(btn);
                     int linha = Grid.GetRow(btn);
 
-                    Debug.WriteLine(btn.Text);
-                    Debug.WriteLine(coluna);
-                    Debug.WriteLine(linha);
+                    teste[linha - 1, coluna] = btn.Text;
+
+                    //Debug.WriteLine(teste);
+
+                    //Debug.WriteLine(btn.Text);
+                    //Debug.WriteLine(coluna);
+                    //Debug.WriteLine(linha);
                 }
             }
 
+            // Visualizando os dados na matriz...
+            for (int i = 0; i < teste.GetLength(0); i++)
+            {
+                for (int j = 0; j < teste.GetLength(1); j++)
+                {
+                    Debug.WriteLine("------------------------------");
+                    Debug.WriteLine(teste[i, j]);
+                }
+            }
         }
     }
 }
